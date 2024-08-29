d6240eb2e384918de5433757e735342b5b57dddd
Minirick <duvanestivencardozo@gmail.com>
2024-08-29 03:32:00 -0500

    refactor: token JWT

--- src/helpers/utils.py
+++ src/helpers/utils.py
@@ -1,74 +1,94 @@
 import os
-from passlib.context import CryptContext
+import uuid
 from datetime import datetime, timedelta
 from typing import Union, Any
+
+from passlib.context import CryptContext
 from jose import jwt, JWTError
 from fastapi import Depends, HTTPException, status
 from sqlalchemy.orm import Session
+
 from src.database.database import get_session
 from src.models.userModel import User
 from src.models.rolModel import UsuarioFincaRol, Rol
 from src.models.permissionModel import Permission, RolPermiso
 from src.helpers.auth_bearer import JWTBearer
-from src.helpers.config import JWT_SECRET_KEY, JWT_REFRESH_SECRET_KEY, ALGORITHM, ACCESS_TOKEN_EXPIRE_MINUTES, REFRESH_TOKEN_EXPIRE_MINUTES
-# Configuración de hashing de contraseñas
+from src.helpers.config import (
+    JWT_SECRET_KEY,
+    JWT_REFRESH_SECRET_KEY,
+    ALGORITHM,
+    ACCESS_TOKEN_EXPIRE_MINUTES,
+    REFRESH_TOKEN_EXPIRE_MINUTES
+)
 
+# Contraseñas
 passwordContext = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 def get_hashed_password(password: str) -> str:
+    """Genera un hash para la contraseña proporcionada."""
     return passwordContext.hash(password)
 
-
 def verify_password(password: str, hashed_pass: str) -> bool:
+    """Verifica si la contraseña coincide con el hash."""
     return passwordContext.verify(password, hashed_pass)
 
+
+def generate_password_reset_token() -> str:
+    return str(uuid.uuid4())
+
+
+def send_password_reset_email(email: str, token: str):
+    print(f"Se ha enviado un correo electrónico a {email} con el token de restablecimiento de contraseña: {token}")
+
+
+#Tokens
 def create_access_token(subject: Union[str, Any], expiresDelta: int = None) -> str:
-    if expiresDelta is not None:
-        expiresDelta = datetime.utcnow() + expiresDelta
-        
+    if expiresDelta:
+        expire = datetime.utcnow() + expiresDelta
     else:
-        expiresDelta = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
-         
+        expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
     
-    to_encode = {"exp": expiresDelta, "sub": str(subject)}
-    encodedJwt = jwt.encode(to_encode, JWT_SECRET_KEY, ALGORITHM)
-     
+    to_encode = {"exp": expire, "sub": str(subject)}
+    encodedJwt = jwt.encode(to_encode, JWT_SECRET_KEY, algorithm=ALGORITHM)
     return encodedJwt
 
 def create_refresh_token(subject: Union[str, Any], expiresDelta: int = None) -> str:
-    if expiresDelta is not None:
-        expiresDelta = datetime.utcnow() + expiresDelta
+    """Crea un token de actualización JWT."""
+    if expiresDelta:
+        expire = datetime.utcnow() + expiresDelta
     else:
-        expiresDelta = datetime.utcnow() + timedelta(minutes=REFRESH_TOKEN_EXPIRE_MINUTES)
+        expire = datetime.utcnow() + timedelta(minutes=REFRESH_TOKEN_EXPIRE_MINUTES)
     
-    toEncode = {"exp": expiresDelta, "sub": str(subject)}
-    encodedJwt = jwt.encode(toEncode, JWT_REFRESH_SECRET_KEY, ALGORITHM)
+    toEncode = {"exp": expire, "sub": str(subject)}
+    encodedJwt = jwt.encode(toEncode, JWT_REFRESH_SECRET_KEY, algorithm=ALGORITHM)
     return encodedJwt
 
-import uuid
 
-def generate_password_reset_token():
-    return str(uuid.uuid4())
-
-def send_password_reset_email(email: str, token: str):
-    # Esto es solo un ejemplo. Deberías integrar un servicio real de envío de correos.
-    print(f"Se ha enviado un correo electrónico a {email} con el token de restablecimiento de contraseña: {token}")
-
-def get_current_user(token: str = Depends(JWTBearer()), db: Session = Depends(get_session)):
+#Obtiene el usuario actual autenticado utilizando el token JWT
+def get_current_user(token: str = Depends(JWTBearer()), db: Session = Depends(get_session)) -> User:
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Invalid authentication credentials"
+    )
+    
     try:
         payload = jwt.decode(token, JWT_SECRET_KEY, algorithms=[ALGORITHM])
         user_id: str = payload.get("sub")
         if user_id is None:
-            raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid authentication credentials")
+            raise credentials_exception
     except JWTError:
-        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid authentication credentials")
+        raise credentials_exception
     
     user = db.query(User).filter(User.id == user_id).first()
     if user is None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="User not found")
+    
     return user
 
+
+#Permisos
 def verify_permission(permission_name: str):
+    """Verifica si el usuario actual tiene el permiso necesario."""
     def verify(user: User = Depends(get_current_user), db: Session = Depends(get_session)):
         # Obtener todos los roles del usuario
         user_roles = db.query(UsuarioFincaRol).filter(UsuarioFincaRol.usuario_id == user.id).all()
@@ -98,4 +118,3 @@ def verify_permission(permission_name: str):
         print(f"Permiso '{permission_name}' no encontrado para el usuario {user.id}")
         raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Not enough permissions")
     return verify
-
