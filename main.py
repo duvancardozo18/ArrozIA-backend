e84d2f698ee9b25b031ae3d8f6f56d4f4fe010ad
Niormon <74582145+Niormon@users.noreply.github.com>
2024-09-13 17:23:56 -0500

    Feat: Consultar cultivos a partir del id del lote

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-
 from src.database.database import Base, engine
 from src.routes.cropRoutes import CROP_ROUTES
+from src.routes.farmRoleRoutes import USER_FARM_ROL_ROUTES
 from src.routes.farmRoutes import FARM_ROUTES
 from src.routes.landRoutes import LAND_ROUTES
 from src.routes.user_farmRoutes import USER_FARM_ROUTES
@@ -13,13 +13,21 @@ from src.routes.roleRoutes import ROLE_ROUTES
 from src.routes.unidadesAreasRoutes import UNIDAD_AREA_ROUTE
 from src.routes.userRouter import USER_ROUTES
 from src.routes.varietyArrozRoutes import VARIETY_ARROZ_ROUTES
+from src.routes.farmLotRoutes import FARM_LOT_ROUTES
+from src.routes.landCropRoutes import LAND_CROP_ROUTES
+from dotenv import load_dotenv
+import os
+
+# Cargar variables de entorno desde el archivo .env
+load_dotenv()
+
 
 # Crear las tablas en la base de datos
 Base.metadata.create_all(engine)
 
 # Inicializar la aplicación FastAPI
 app = FastAPI()
- 
+
 # Incluir las rutas de usuario
 app.include_router(USER_ROUTES)
 
@@ -53,12 +61,24 @@ app.include_router(VARIETY_ARROZ_ROUTES, prefix="/varieties", tags=["Varieties o
 # Incluir las rutas para cultivo
 app.include_router(CROP_ROUTES)
 
+# Incluir las rutas para las cosechas vinculadas a los lotes
+app.include_router(LAND_CROP_ROUTES)
+
+# Incluir las rutas para lotes de la finca
+app.include_router(FARM_LOT_ROUTES)  # Añadir esta línea
+
+# Incluir las rutas para USUARIO FINCA ROLE
+app.include_router(USER_FARM_ROL_ROUTES)
+
+# Obtener las direcciones permitidas desde las variables de entorno
+allow_origins = os.getenv("ALLOW_ORIGINS", "").split(",")
+
+
 # Configuración del middleware de CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],  # Permite solicitudes solo desde el frontend en localhost:3000
+    allow_origins=allow_origins,  # Permite solicitudes desde las URLs especificadas en la variable de entorno
     allow_credentials=True,
     allow_methods=["*"],  # Permite todos los métodos HTTP (GET, POST, etc.)
     allow_headers=["*"],  # Permite todos los encabezados
 )
-
