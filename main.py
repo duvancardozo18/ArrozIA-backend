9dae759bf79fdb38fe6e47450b9d0196f1883a21
Duban Estiben Cardozo Osorio <60492285+Minirick@users.noreply.github.com>
2024-09-23 17:43:49 -0500

    fix: ruta de inicio de sesion

--- main.py
+++ main.py
@@ -21,11 +21,8 @@ from src.routes.varietyArrozRoutes import VARIETY_ARROZ_ROUTES
 
 from src.helpers.utils import get_current_user
 
-# Inicializar la aplicación FastAPI
-#app = FastAPI()
-app = FastAPI(
-    dependencies=[Depends(get_current_user)]  # Protege todas las rutas con autenticación
-)
+# Inicializar la aplicación FastAPI sin protección global
+app = FastAPI()
 
 # Cargar variables de entorno desde el archivo .env
 load_dotenv()
@@ -33,24 +30,28 @@ load_dotenv()
 # Crear las tablas en la base de datos
 Base.metadata.create_all(engine)
 
-# Incluir Rutas
+# Ruta raíz sin protección
 @app.get("/")
 def read_root():
     return {"message": "Welcome to my API"}
+
+# Incluir las rutas de autenticación sin protección
 app.include_router(AUTH_ROUTES)
+
+# Incluir rutas protegidas con autenticación
 app.include_router(USER_ROUTES)
-app.include_router(ROLE_ROUTES)
-app.include_router(PERMISSION_ROUTES)
-app.include_router(ROL_PERMISSION_ROUTES)
-app.include_router(FARM_ROUTES)
-app.include_router(USER_FARM_ROUTES)
-app.include_router(LAND_ROUTES)
-app.include_router(UNIDAD_AREA_ROUTE)
-app.include_router(VARIETY_ARROZ_ROUTES)
-app.include_router(CROP_ROUTES)
-app.include_router(LAND_CROP_ROUTES)
-app.include_router(FARM_LOT_ROUTES)
-app.include_router(PASSWORD_RESET_ROUTES)  
+app.include_router(ROLE_ROUTES, dependencies=[Depends(get_current_user)])
+app.include_router(PERMISSION_ROUTES, dependencies=[Depends(get_current_user)])
+app.include_router(ROL_PERMISSION_ROUTES, dependencies=[Depends(get_current_user)])
+app.include_router(FARM_ROUTES, dependencies=[Depends(get_current_user)])
+app.include_router(USER_FARM_ROUTES, dependencies=[Depends(get_current_user)])
+app.include_router(LAND_ROUTES, dependencies=[Depends(get_current_user)])
+app.include_router(UNIDAD_AREA_ROUTE, dependencies=[Depends(get_current_user)])
+app.include_router(VARIETY_ARROZ_ROUTES, dependencies=[Depends(get_current_user)])
+app.include_router(CROP_ROUTES, dependencies=[Depends(get_current_user)])
+app.include_router(LAND_CROP_ROUTES, dependencies=[Depends(get_current_user)])
+app.include_router(FARM_LOT_ROUTES, dependencies=[Depends(get_current_user)])
+app.include_router(PASSWORD_RESET_ROUTES, dependencies=[Depends(get_current_user)])
 
 # Obtener las direcciones permitidas desde las variables de entorno
 allow_origins = os.getenv("ALLOW_ORIGINS", "").split(",")
@@ -59,7 +60,6 @@ allow_origins = os.getenv("ALLOW_ORIGINS", "").split(",")
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
-    #allow_origins=allow_origins,  # Permite solicitudes desde las URLs especificadas en la variable de entorno
     allow_credentials=True,
     allow_methods=["*"],  # Permite todos los métodos HTTP (GET, POST, etc.)
     allow_headers=["*"],  # Permite todos los encabezados
