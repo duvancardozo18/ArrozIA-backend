3103ba64e5b70652937eaf42391028abfc875648
Duban Estiben Cardozo Osorio <60492285+Minirick@users.noreply.github.com>
2024-09-23 21:34:18 -0500

    CORS en Render

--- main.py
+++ main.py
@@ -38,7 +38,8 @@ print("Orígenes permitidos para CORS:", allow_origins)
 # Configuración del middleware de CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=allow_origins,  # Permite solicitudes desde las URLs especificadas en la variable de entorno
+    allow_origins=["*"],  # Permite solicitudes desde las URLs especificadas en la variable de entorno
+    #allow_origins=allow_origins,
     allow_credentials=True,
     allow_methods=["*"],  # Permite todos los métodos HTTP (GET, POST, etc.)
     allow_headers=["*"],  # Permite todos los encabezados
