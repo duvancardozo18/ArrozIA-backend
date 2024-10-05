2bb4227fd1695a63a50cad6d52c4932b7f976b7d
maikercanon <maikersito200109@gmail.com>
2024-10-05 17:20:59 -0500

    feat: nueva rutapara traer id y nombre de variedad de arroz; id y nombre de lote

--- src/models/landModel.py
+++ src/models/landModel.py
@@ -11,13 +11,12 @@ class Land(Base):
     nombre = Column(String(100), nullable=False)
     finca_id = Column(Integer, ForeignKey('finca.id'), nullable=False)
     area = Column(Float, nullable=False)
-    unidad_area_id = Column(Integer, ForeignKey('unidad_area.id'), nullable=False)
     latitud = Column(DECIMAL(10, 5), nullable=True)
     longitud = Column(DECIMAL(10, 5), nullable=True)
     
     # Relaciones
     finca = relationship("Farm", back_populates="lotes")   
-    unidad_area = relationship("UnidadArea", back_populates="lotes")
+    #unidad_area = relationship("UnidadArea", back_populates="lotes")
     crops = relationship("Crop", back_populates="lotes") 
 
 
@@ -28,4 +27,4 @@ class UnidadArea(Base):
     unidad = Column(String(50), nullable=False)
 
     # Relación inversa
-    lotes = relationship("Land", order_by=Land.id, back_populates="unidad_area")
+    #lotes = relationship("Land", order_by=Land.id, back_populates="unidad_area")
