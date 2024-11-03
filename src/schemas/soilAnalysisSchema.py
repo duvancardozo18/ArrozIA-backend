1dba40250167bd615f6cd652f847762e2ee9adc7
Niormon <74582145+Niormon@users.noreply.github.com>
2024-11-03 14:49:43 -0500

    Refactor: Crud Soil Analysis

--- src/schemas/soilAnalysisSchema.py
+++ src/schemas/soilAnalysisSchema.py
@@ -1,21 +1,126 @@
-from pydantic import BaseModel, Field
+from typing import Optional, List
+from pydantic import BaseModel, root_validator
 from datetime import date
-from typing import Optional
 
-class SoilAnalysisBase(BaseModel):
-    fecha_analisis: date
+# Biological Parameter Output
+class BiologicalParamOut(BaseModel):
+    biomasa_microbiana: Optional[float] = None
+    actividad_enzimatica: Optional[float] = None
+
+    model_config = {
+        "from_attributes": True
+    }
+
+# Soil Type Output
+class SoilTypeOut(BaseModel):
+    descripcion: str
+
+    model_config = {
+        "from_attributes": True
+    }
+
+# Land Output
+class LandOut(BaseModel):
+    nombre: str
+
+    model_config = {
+        "from_attributes": True
+    }
+
+# Macronutrient Output
+class MacronutrientOut(BaseModel):
+    n: Optional[float] = None
+    p: Optional[float] = None
+    k: Optional[float] = None
+    ca: Optional[float] = None
+    mg: Optional[float] = None
+    s: Optional[float] = None
+
+    model_config = {
+        "from_attributes": True
+    }
+
+# Micronutrient Output
+class MicronutrientOut(BaseModel):
+    fe: Optional[float] = None
+    cu: Optional[float] = None
+    mn: Optional[float] = None
+    zn: Optional[float] = None
+    b: Optional[float] = None
+
+    model_config = {
+        "from_attributes": True
+    }
+
+# Chemical Parameter Output
+class ChemicalParamOut(BaseModel):
+    ph: Optional[float] = None
+    conductividad_electrica: Optional[float] = None
+    materia_organica: Optional[float] = None
+    capacidad_intercambio_cationico: Optional[float] = None
+    macronutriente: Optional[List[MacronutrientOut]] = None
+    micronutriente: Optional[List[MicronutrientOut]] = None
+
+    model_config = {
+        "from_attributes": True
+    }
+
+# Physical Parameter Output
+class PhysicalParamOut(BaseModel):
+    textura_id: Optional[int] = None
+    textura_descripcion: Optional[str] = None
+    densidad_aparente: Optional[float] = None
+    profundidad_efectiva: Optional[float] = None
+    color_id: Optional[int] = None
+    color_descripcion: Optional[str] = None
+
+    model_config = {
+        "from_attributes": True
+    }
+
+class SoilAnalysisCreate(BaseModel):
     lote_id: int
+    fecha_analisis: str
     tipo_suelo_id: int
-    archivo_reporte: Optional[bytes] = None
+    archivo_reporte: Optional[str] = None
+    parametro_biologico: Optional[BiologicalParamOut] = None
+    parametro_quimico: Optional[ChemicalParamOut] = None
+    parametro_fisico: Optional[PhysicalParamOut] = None
 
-class SoilAnalysisCreate(SoilAnalysisBase):
-    pass
+    model_config = {
+        "from_attributes": True
+    }
 
-class SoilAnalysisUpdate(SoilAnalysisBase):
-    pass
-
-class SoilAnalysisOut(SoilAnalysisBase):
+# Soil Analysis Output
+class SoilAnalysisOut(BaseModel):
     id: int
+    fecha_analisis: str
+    soil_type: Optional[SoilTypeOut] = None
+    lote: Optional[LandOut] = None
+    archivo_reporte: Optional[str] = None
+    parametro_biologico: Optional[BiologicalParamOut] = None
+    parametro_quimico: Optional[ChemicalParamOut] = None
+    parametro_fisico: Optional[PhysicalParamOut] = None
 
-    class Config:
-        orm_mode = True
+    model_config = {
+        "from_attributes": True
+    }
+
+    @root_validator(pre=True)
+    def format_fecha_analisis(cls, values):
+        # Intentar obtener `fecha_analisis` del objeto directamente
+        fecha_analisis = getattr(values, "fecha_analisis", None)
+        
+        # Si `fecha_analisis` es de tipo `date`, formatearlo
+        if isinstance(fecha_analisis, date):
+            values.fecha_analisis = fecha_analisis.strftime("%Y-%m-%d")
+        return values
+    
+
+class SoilAnalysisSimpleOut(BaseModel):
+    id: int
+    message: str
+
+    model_config = {
+        "from_attributes": True
+    }
